cea61294502c26c8e871f1d1cd9de29da4c960d0
Roland Kajatin <roland.kajatin@gmail.com>
2022-09-10 22:12:20 +0200

    Initial Commit

---
+++ Aqua/AquaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaApp.swift
+//  Aqua
+//
+//  Created by Roland Kajatin on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AquaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
