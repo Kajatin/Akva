85ef40a11d7758893a5c4a0621c5dc5b7bbcd2bc
Roland Kajatin <roland.kajatin@gmail.com>
2023-06-24 22:46:51 +0200

    watchos widget extension

---
+++ WatchWidgets/WatchWidgets.swift
@@ -0,0 +1,18 @@
+//
+//  WatchWidgets.swift
+//  WatchWidgets
+//
+//  Created by Roland Kajatin on 24/06/2023.
+//
+
+import SwiftUI
+import SwiftData
+import WidgetKit
+import DiateryWaterData
+
+@main
+struct WatchWidgets: WidgetBundle {
+    var body: some Widget {
+        AkvaWidget()
+    }
+}
